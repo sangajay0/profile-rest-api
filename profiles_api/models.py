3e8886cf8b552f85419ba4cd5121b3a2bb82cb91
jay <jaysanga@outlook.com>
2022-12-18 18:58:48 +0530

    Configured Vagarant and setup hello script

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionMixin
+from  django.contrib.auth.models import BaseUseManager
+
+
+class UserProfileManager(BaseUseManager):
+    """manager for usre profile"""
+
+    def create_user(self,email,password=None):
+        """Create a new user profile""""
+        if not email:
+            raise ValueError('User muust have an email address')
+        email =self.normalize_email(email)
+        user=self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self,email,name,password):
+        """Create and save a new superuper  with  given deatails """
+        user =self.create_user(email,name, password)
+
+        user.is_superuser=True
+        user.is_staff =True
+        user.save(using=self._db)
+         return create_superuser
+
+
+
+
+
+class UserProfile(AbstractBaseUser,PermissionMixin):
+    """Database model for user in the system."""
+    email = models.EmailField(max_length=225, unique=True)
+    name=models.CharField(max_length=225)
+    is_activate =models.BooleanField(default=True)
+     is_staff = models.BooleanField(default=False)
+
+     objects=UserProfileManager()
+
+     USERNAME_FIELD='email'
+     REQUIRED_FIELDS='name'
+
+     def get_full_name(self):
+         """"Retrive full name  of user """
+         return self.name
+    def get_short_name(self):
+        """Retrive short name of user"""
+        return self.name
+
+    def _str__(self):
+        """Return string reperesentation of our user """
+        return self.email
+
+
 
 # Create your models here.
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 ROOT_URLCONF = 'profiles_project.urls'
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL="'profiles_api.UserProfile'
